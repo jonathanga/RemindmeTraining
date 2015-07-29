e6830d716d2b703aa12887f0881371bca51feda0
carmanu <litos125@hotmail.com>
2015-07-29 18:03:43 -0500

    Facebook Terminado

--- app/src/main/java/co/edu/sena/jonathan/remindmetraining/DetalleActivity.java
+++ app/src/main/java/co/edu/sena/jonathan/remindmetraining/DetalleActivity.java
@@ -1,20 +1,42 @@
 package co.edu.sena.jonathan.remindmetraining;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.facebook.FacebookSdk;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+
 
 public class DetalleActivity extends Activity {
+
+
+    // TODO  Colocar la Imagen el BITMAP imagenMostrada
+    Bitmap imagenMostrada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_detalle);
+
+        // TODO  Colocar la Imagen el BITMAP imagenMostrada
     }
 
     public void eliminarFoto(View view) {
+    }
+
+    public void compartirFacebook(View view) {
+        // TODO  Colocar la Imagen el BITMAP imagenMostrada
+        SharePhoto photo = new SharePhoto.Builder()
+                .setBitmap(imagenMostrada)
+                .build();
+        SharePhotoContent content = new SharePhotoContent.Builder()
+                .addPhoto(photo)
+                .build();
     }
 }
