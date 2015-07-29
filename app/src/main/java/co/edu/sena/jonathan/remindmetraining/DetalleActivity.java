c6dbe95f9a93d5e31d15e15addd5edb9c188c480
carmanu <litos125@hotmail.com>
2015-07-29 17:40:45 -0500

    Diseño de interfaces terminado - iniciando con facebook

---
+++ app/src/main/java/co/edu/sena/jonathan/remindmetraining/DetalleActivity.java
@@ -0,0 +1,20 @@
+package co.edu.sena.jonathan.remindmetraining;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class DetalleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle);
+    }
+
+    public void eliminarFoto(View view) {
+    }
+}
